02d1695b5a4c93e59c255b67db983481624900c9
Jorge <91492937+jorgeloar@users.noreply.github.com>
2026-01-29 13:57:56 +0100

    Update App.jsx
    cambio de webhook

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const UgcScriptGenerator = () => {
   const [copied, setCopied] = useState(false);
   
   // Tu Webhook de Make (Configurado por defecto)
-  const DEFAULT_WEBHOOK = "https://hook.eu2.make.com/c5s90k7am0gxc1286y64x0sxonidipzx";
+  const DEFAULT_WEBHOOK = "https://hook.eu2.make.com/ntnsv551dqcromn7qyvl4k1olqwybjh7";
   const [manualWebhook, setManualWebhook] = useState(DEFAULT_WEBHOOK);
 
   const handleInputChange = (e) => {
